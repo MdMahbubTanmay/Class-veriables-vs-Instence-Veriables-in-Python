b87f11a9c799cebe32b4527e9e866df33cc0eec7
Md Mahbub Tanmay <129056181+MdMahbubTanmay@users.noreply.github.com>
2025-06-26 19:42:33 +0600

    Update main.py

--- main.py
+++ main.py
@@ -24,3 +24,44 @@ x.Num = 9
 print(x.Num,x.health) #9 99
 
 print(Main.Num,Main.health) #66 100 (same as line 16)
+
+
+
+
+
+
+#************ So The question is is there any way using which we can change class global variable
+# without using class like Main.health = 7575  with using instance like x.health = 7575?
+#The answer is yes. 
+
+
+
+class Main2:
+        health = None
+
+        def GlobalChange(self,h): # it will change k.health value not global health value
+             self.health = h
+
+        @classmethod #Now it will change global health value but it will have no effect on instance like x.health 
+        def GlobalChange2(self,h): 
+             self.health = h
+
+
+k = Main2()
+k.health = 33
+
+print(k.health) #Changed
+print(Main2.health) #Unchanged
+
+
+
+
+k.GlobalChange(6969)
+print(k.health) #Changed
+print(Main2.health) #Unchanged
+
+
+#Lets try that @classmethod
+k.GlobalChange2(7575)
+print(k.health) # Unchanged
+print(Main2.health) # Changed
